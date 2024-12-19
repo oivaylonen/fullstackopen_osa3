1dfb1bbff90e819204e5261f0cdb45409e64e2f8
Oiva Ylönen <oiva.ylonen@gmail.com>
2024-12-19 14:58:12 +0200

    Muita muutoksia

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
+app.use(express.static('dist'))
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 
 const cors = require('cors')
 app.use(cors())
